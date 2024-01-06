517ef2beafb4de4a76f0e1f03ba5f526c1053ac4
Vasilina <vasilinamel04@gmail.com>
2024-01-06 23:55:48 +1000

    Создан проект, добавлена canva, рисование

---
+++ draw-app/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <drawing-canvas />
+  </div>
+</template>
+
+<script>
+
+import DrawingCanvas from './components/DrawCanva.vue'
+
+export default {
+  name: 'App',
+  components: {
+    DrawingCanvas
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ draw-app/src/components/DrawCanva.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <div>
+      <input type="color" v-model="lineColor" />
+      <input type="range" min="1" max="20" v-model="lineWidth" />
+      <button @click="clearCanvas">Clear</button>
+    </div>
+    <canvas ref="canvas" :width="800" :height="600" @mousedown="startDrawing" @mousemove="draw" @mouseup="endDrawing" @touchstart="startDrawing" @touchmove="draw" @touchend="endDrawing" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DrawingCanvas',
+  data() {
+    return {
+      isDrawing: false,
+      context: null,
+      lineColor: '#000000', // Цвет по умолчанию
+      lineWidth: 5 // Толщина линии по умолчанию
+    }
+  },
+  methods: {
+    startDrawing(event) {
+      this.isDrawing = true;
+      const canvas = this.$refs.canvas;
+      this.context = canvas.getContext('2d');
+      this.context.lineWidth = this.lineWidth;
+      this.context.strokeStyle = this.lineColor;
+      const x = event.type === 'touchstart' ? event.touches[0].clientX - canvas.offsetLeft : event.offsetX;
+      const y = event.type === 'touchstart' ? event.touches[0].clientY - canvas.offsetTop : event.offsetY;
+      this.context.beginPath();
+      this.context.moveTo(x, y);
+    },
+    draw(event) {
+      if (this.isDrawing) {
+        const canvas = this.$refs.canvas;
+        const x = event.type === 'touchmove' ? event.touches[0].clientX - canvas.offsetLeft : event.offsetX;
+        const y = event.type === 'touchmove' ? event.touches[0].clientY - canvas.offsetTop : event.offsetY;
+        this.context.lineTo(x, y);
+        this.context.stroke();
+      }
+    },
+    endDrawing() {
+      this.isDrawing = false;
+    },
+    clearCanvas() {
+      const canvas = this.$refs.canvas;
+      const context = canvas.getContext('2d');
+      context.clearRect(0, 0, canvas.width, canvas.height);
+    }
+  }
+}
+</script>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
