0720629ffe99cd63af43fcc5a3a5391e17572e39
Vasilina <vasilinamel04@gmail.com>
2024-01-07 14:29:52 +1000

    Сделана основная логика и открытие всех всплывающих окон

--- draw-app/src/components/Draw.vue
+++ draw-app/src/components/Draw.vue
@@ -1,28 +1,37 @@
 <template>
   <div>
-    <button @click="openFullscreenPopup" v-if="!gameStarted && !fullscreenPopupVisible">Правила игры</button>
+    <button @click="openFullscreenPopup" v-if="!gameStarted && !fullscreenPopupVisible && !drawingSubjectPromptVisible">Правила игры</button>
     <div v-if="fullscreenPopupVisible" class="fullscreen-popup">
       <h2>Название игры</h2>
       <p>Тут будут правила вашей игры...</p>
       <button @click="closeFullscreenPopup">Закрыть</button>
     </div>
   </div>
-  <div v-if="!gameStarted && !fullscreenPopupVisible">
+  <div v-if="!gameStarted && !fullscreenPopupVisible && !drawingSubjectPromptVisible">
     <img alt="logo" src="../assets/img.png">
-    <button @click="startGame" v-if="!gameStarted">Начать игру</button>
   </div>
-    <div v-if="gameStarted">
+    <div v-if="gameStarted && !fullscreenPopupVisible">
       <div>
         <p>Оставшееся время: {{ countdownTime }}</p>
+         <p>Нарисуйте предмет {{ selectedDrawingSubject }}</p>
         <input type="color" v-model="lineColor" />
         <input type="range" min="1" max="20" v-model="lineWidth" />
         <button @click="clearCanvas">Clear</button>
         <button @click="endGame" v-if="gameStarted">Выход</button>
+        <button @click="openPromptForDrawingSubject">Следующий рисунок</button>
       </div>
       <canvas ref="canvas" :width="1200" :height="1200" @mousedown="startDrawing" @mousemove="draw" @mouseup="endDrawing" @touchstart="startDrawing" @touchmove="draw" @touchend="endDrawing" />
-      <div class="canvas-container">
-      </div>
     </div>
+
+  <div>
+    <div v-if="!gameStarted && !drawingSubjectPromptVisible && !fullscreenPopupVisible">
+      <button @click="openPromptForDrawingSubject">Начать игру</button>
+    </div>
+    <div v-if="drawingSubjectPromptVisible && !gameStarted && !fullscreenPopupVisible" class="prompt-dialog">
+      <p>Нарисуйте предмет {{ selectedDrawingSubject }} за 20 секунд</p>
+      <button @click="startGame">Хорошо</button>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -40,7 +49,10 @@ export default {
       lineWidth: 5, // Толщина линии по умолчанию
       fullscreenPopupVisible: false,
       countdownTime: 20,
-      timer: null
+      timer: null,
+      drawingSubjectPromptVisible: false,
+      selectedDrawingSubject: '',
+      drawingSubjects: ['Дом', 'Дерево', 'Солнце', "Ёж", "Медведь", "Облако"]
     };
   },
   methods: {
@@ -66,6 +78,7 @@ export default {
     },
     endGame() {
       this.gameStarted = false;
+      this.drawingSubjectPromptVisible = false;
       clearInterval(this.timer);
     },
      startDrawing(event) {
@@ -101,8 +114,25 @@ export default {
         this.countdownTime--;
       } else {
         this.endGame();
+        const randomIndex = Math.floor(Math.random() * this.drawingSubjects.length);
+        const randomSubject = this.drawingSubjects[randomIndex];
+        this.showDrawingPrompt(randomSubject);
       }
-    }
+    },
+    showDrawingPrompt(subject) {
+  this.drawingSubjectPromptVisible = true;
+  this.selectedDrawingSubject = subject;
+  },
+    openPromptForDrawingSubject() {
+      const randomIndex = Math.floor(Math.random() * this.drawingSubjects.length);
+      this.selectedDrawingSubject = this.drawingSubjects[randomIndex];
+      this.drawingSubjectPromptVisible = true;
+    },
+    openNextLevelPrompt() {
+      const randomIndex = Math.floor(Math.random() * this.drawingSubjects.length);
+      const randomSubject = this.drawingSubjects[randomIndex];
+      this.showDrawingPrompt(randomSubject);
+    },
   }
 };
 </script>
